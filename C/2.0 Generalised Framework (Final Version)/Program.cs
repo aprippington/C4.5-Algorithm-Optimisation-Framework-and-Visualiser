97dc72e67997059045d5b5c196758d51df1fd510
Rippington, Andrew P <ar17148@essex.ac.uk>
2020-04-21 10:08:35 +0100

    Upload New File

---
+++ C/2.0 Generalised Framework (Final Version)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decision_Tree_Optimisation_Generalised
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            DecisionTree t = new DecisionTree();
+            t.runProgram();
+        }
+    }
+}
+
+
